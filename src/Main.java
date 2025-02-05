5c5be4fc2e090534c03424f87446fafdc971b664
Kurilenko Aleksandr <me@nobito.ru>
2025-02-05 18:02:28 +0300

    some exceptios IOException  in method deleted

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Main {
     static Date localDateTime = new Date();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         StringBuilder sb = new StringBuilder();
 
